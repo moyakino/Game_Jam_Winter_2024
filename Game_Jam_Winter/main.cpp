98055f706d84d67d79629c953d54f9d13eaff8a8
moyakino <104491816+moyakino@users.noreply.github.com>
2024-02-12 01:39:52 +0900

    main作成

--- Game_Jam_Winter/main.cpp
+++ Game_Jam_Winter/main.cpp
@@ -1,1 +1,36 @@
-﻿
+﻿#include "DxLib.h"
+
+#define SCREEN_WIDTH 1280 //横の長さ
+#define SCREEN_HEIGHT 720 //縦の長さ
+
+// プログラムは WinMain から始まります
+int WINAPI WinMain(_In_ HINSTANCE  hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR LpCmdLine, _In_ int NCmdShow)
+{
+	//タイトル
+	SetMainWindowText("Game_Jam_Winter");
+
+	//Log.txtを生成しないようにする
+	SetOutApplicationLogValidFlag(FALSE);
+
+	//ウィンドウモードで起動
+	ChangeWindowMode(TRUE);
+
+	//画面サイズの設定
+	SetGraphMode(SCREEN_WIDTH, SCREEN_HEIGHT, 32);
+
+	//DXライブラリの初期化処理
+	if (DxLib_Init() == -1)return -1;
+
+	//描画先を裏にする
+	SetDrawScreen(DX_SCREEN_BACK);
+
+	if (DxLib_Init() == -1) // ＤＸライブラリ初期化処理
+	{
+		return -1; // エラーが起きたら直ちに終了
+	}
+
+	DrawPixel(320, 240, GetColor(255, 255, 255)); // 点を打つ
+	WaitKey(); // キー入力待ち
+	DxLib_End(); // ＤＸライブラリ使用の終了処理
+	return 0; // ソフトの終了
+}
